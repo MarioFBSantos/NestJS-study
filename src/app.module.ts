a80d93789ec59a9c5044ed1505c44b7e49c0c78b
MarioFBSantos <mariosantos3197@gmail.com>
2022-09-07 18:50:24 -0300

    creating connection but using .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Mongoose } from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [Mongoose],
+  imports: [MongooseModule.forRoot(process.env.MAIN_MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
